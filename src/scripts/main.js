91d868b66cff899dff1b918ae33588c8d5462f90
Michael Phillips <mphillips242001@outlook.com>
2025-05-27 15:06:11 -0500

    Starting harvester module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,30 @@
-console.log("Welcome to the main module")
+import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+import {createCorn} from "./seeds/corn.js"
+import {createPotato} from "./seeds/potato.js"
+import {createSoybean} from "./seeds/soybean.js"
+import {createSunflower} from "./seeds/sunflower.js"
+import {createWheat} from "./seeds/wheat.js"
+import { usePlants,addPlant } from "./fields.js"
+const yearlyPlan = createPlan()
+//console.log(yearlyPlan)
 
+const asparagusSeed = createAsparagus()
+const cornSeed = createCorn()
+const potatoSeed = createPotato()
+const soybeanSeed = createSoybean()
+const sunflowerSeed = createSunflower()
+const wheatSeed = createWheat()
+
+addPlant(asparagusSeed)
+addPlant(cornSeed)
+addPlant(potatoSeed)
+addPlant(soybeanSeed)
+addPlant(sunflowerSeed)
+addPlant(wheatSeed)
+
+const plantsInField = usePlants()
+
+console.log(plantsInField)
+
+//console.log(asparagusSeed)
