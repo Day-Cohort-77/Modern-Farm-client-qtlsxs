76eba862eede5e5341d9f7d3b6e169fee64b7e1d
Bedon Lancaster <bedonlancaster@gmail.com>
2025-05-29 14:21:06 -0500

    now we got two options of displaying our data

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,17 +1,36 @@
 // Define and export a `Catalog` function.
 //1. The Catalog function should accept the harvested food array as input.
 // The Catalog function should iterate the array of food objects.
- //As you iterate the array, the `<main>` element should have its inner HTML appended with an HTML representation of a food item. Use the following example for each food item. The example has Corn hard-coded. Yours should not. It should output the name property of the current food object.
+//As you iterate the array, the `<main>` element should have its inner HTML appended with an HTML representation of a food item. Use the following example for each food item. The example has Corn hard-coded. Yours should not. It should output the name property of the current food object.
 
-    
 
-    export const catalog= (harvest) => {
-        let harvestHtml = ""
-        for (const food of harvest) {
-            harvestHtml += `<section class="plant">${food.type}</section>`
-            
 
-            
-        }
-return harvestHtml
+export const catalog = (harvest) => {
+    let harvestHtml = ""
+    for (const food of harvest) {
+        harvestHtml += `<section class="plant">${food.type}</section>`
+
+
+
     }
+    return harvestHtml
+}
+
+export const catalogTwo = (harvest) => {
+    // Count each plant type
+    const counts = {};
+    for (const plant of harvest) {
+        if (counts[plant.type]) {
+            counts[plant.type]++;
+        } else {
+            counts[plant.type] = 1;
+        }
+    }
+    // Build the table HTML
+    let html = "<table><tr><th>Food</th><th>Quantity</th></tr>";
+    for (const type in counts) {
+        html += `<tr><td>${type}</td><td>${counts[type]}</td></tr>`;
+    }
+    html += "</table>";
+    return html;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,14 @@ import { createPlan } from "./plan.js"
 import { plantSeeds } from "./tractor.js"
 import { usePlants } from "./field.js"
 import { harvestPlants } from "./harvester.js"
-import { catalog } from "./catalog.js"
+import { catalog, catalogTwo } from "./catalog.js"
 const yearlyPlan = createPlan()
 plantSeeds(yearlyPlan)
 
 const plantsInField = usePlants()
-//console.log(plantsInField)
+
 
 const harvestedPlant = harvestPlants(plantsInField)
 harvestedPlant.sort((a, b) => a.type.localeCompare(b.type))
-document.querySelector(".container").innerHTML = catalog(harvestedPlant)
+//document.querySelector(".container").innerHTML = catalog(harvestedPlant)
+document.querySelector(".container").innerHTML = catalogTwo(harvestedPlant)
